2f5162139c4b6078ba9cea7cadc91cae7c947c51
01320Nabi <yangeh2225@gmail.com>
2021-01-19 20:22:17 +0900

    PlayerController Script added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private Rigidbody rigidbody;
-
+    private new Rigidbody rigidbody;
+    public new Camera camera;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    private Vector3 moveVector3;
+
     void Update()
     {
-        
+        moveVector3.x = Input.GetAxisRaw("Horizontal");
+        moveVector3.z = Input.GetAxisRaw("Vertical");
+        moveVector3 = moveVector3.normalized * speed;
+        moveVector3 = Quaternion.Euler(Vector3.up * camera.transform.rotation.eulerAngles.y) * moveVector3;
+    }
+
+    void LateUpdate()
+    {
+        Move();
     }
 
     public void Move()
     {
-        rigidbody.MovePosition(Vector3)
+        rigidbody.MovePosition(rigidbody.position + moveVector3 * Time.deltaTime);
     }
 }
