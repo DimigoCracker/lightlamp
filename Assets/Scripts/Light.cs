fbe050b66c864d9d5f823a203aaf01d9911aeccd
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-01-20 12:12:04 +0900

    추가 : 전등
    클리어 지점의 역할인 전등을 만들었다. 테그가 플레이어인 물체와 충돌할때 클리어신으로 이동한다. 추후 사용할때 컴포넌트의 clearSceneName에 클리어 신의 이름을 넣어준다.

---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Light : MonoBehaviour
+{
+    public string clearSceneNmae;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(clearSceneNmae);
+        }
+    }
+}
