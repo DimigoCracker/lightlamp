b7476915422e6cbf5f355fcbd9d9a4053e6129a7
YuSanguk <75666980+YuSanguk@users.noreply.github.com>
2021-01-24 19:47:07 +0900

    House1 - Trampoline 제작
    카메라 위치 옮겨져있음
    스크립트에 설명문이라는 이름으로 수정사항 등이 적혀있음

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    bool willBounce = false;
+    float bounceHeight = -0.7f;
+    public Transform Player;
+    float velo;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 velocity = Player.GetComponent<Rigidbody>().velocity;
+        if (velocity.y != 0)
+            velo = velocity.y;
+        if (willBounce)
+        {
+            Debug.Log(velo);
+            Player.GetComponent<Rigidbody>().velocity = new Vector3(velocity.x, 0, velocity.z);
+            Player.GetComponent<Rigidbody>().AddForce(0, velo * bounceHeight, 0, ForceMode.Impulse);
+            willBounce = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Trampoline_tag")
+        {
+            willBounce = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/설명문.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Trampoline에 있던 Capsule Collider 삭제
+        그거 때문에 공중에 둥둥떠있어서 10분낭비당함 ㅠ...
+        대신 Trmapoline_up에 Mesh Collider 넣어둠 
+    Trampoline_up을 만듬
+        Trampoline에 그냥 넣으면 옆면에 닿아도 y값 백터를 받기때문에 그냥 Trampoline위에 얆은
+        막을 덮어둠. 
+    Trampoline_up에 리지드바디 추가
+        리지드바디 설정은 InKinematic을 켜놨고, 중력영향은 꺼둠
+    Tag추가
+        Trampoline_Tag라는 것을 만듬
+    어떻게 만들었냐?
+        Player에 Tranpoline이라는 스크립트를 붙혔고, 그 효과는 Tag가 Trampoline_Tag라는 것에
+        닿을시 위로 y값 백터를 얻는 방식
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 설명문 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
