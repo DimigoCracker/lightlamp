f4980ff92330938cf6f3a75034519de9d8e3dd6e
header1213 <tworiver1213@naver.com>
2022-03-21 08:33:57 +0900

    byebye

---
+++ Assets/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject PlayerCharacter;
+    public Transform CameraArm;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void CameraControll() {
+        Vector2 MouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 CameraAngles = CameraArm.rotation.eulerAngles;
+
+        CameraArm.rotation = Quaternion.Euler(CameraAngles.x - MouseInput.y, CameraAngles.y + MouseInput.x, CameraAngles.z);
+    }
+}
